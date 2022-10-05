a679b68bc4a139665d04887967ba6bce346acf19
ParkDoil <103029337+ParkDoil@users.noreply.github.com>
2022-10-05 15:10:40 +0900

    [FEAT] Weapon division / 무기 배분

---
+++ 22.10.04/Assets/Scripts/Manager/WeaponManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] _robotArms;
+
+    private GameObject[] _weapons;
+    private int _childrenCount;
+    private bool[] _isSelect;
+
+    private void Awake()
+    {
+        _childrenCount = transform.childCount;
+        _weapons = new GameObject[_childrenCount];
+        _isSelect = new bool[_childrenCount];
+
+        for (int i = 0; i < _childrenCount; ++i)
+        {
+            _weapons[i] = transform.GetChild(i).gameObject;
+            _isSelect[i] = false;
+        }
+
+        for (int i = 0; i < _robotArms.Length;)
+        {
+            int _randomnum = Random.Range(0, _childrenCount);
+            Debug.Log(_randomnum);
+            if (_isSelect[_randomnum] == false)
+            {
+                _isSelect[_randomnum] = true;
+                _weapons[_randomnum].transform.SetParent(_robotArms[i].transform);
+                ++i;
+            }
+        }
+    }
+}
