e91363411ec1bae6ce8d5751cad8a7797325a06d
Park Doil <poker5323@naver.com>
2023-01-04 12:56:36 +0900

    [Feat] 큐브 오브젝트 풀링

--- 23.01.04_MapTool/Assets/Scripts/CubeObjectPool.cs
+++ 23.01.04_MapTool/Assets/Scripts/CubeObjectPool.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class CubeObjectPool : MonoBehaviour
 {
     [SerializeField] private GameObject _cube;
+    [SerializeField] private GameObject _parentObject;
 
-    public GameObject[] ObjectPool { get; set; }
+    public GameObject[] ObjectPool; //{ get; set; }
 
-    private void wake()
+    private void Awake()
     {
-        for(int i = 0;i<400;i++)
-        {
+        ObjectPool = new GameObject[400];
 
+        for (int i = 0; i < 400; i++)
+        {
+            ObjectPool[i] = Instantiate(_cube);
+            ObjectPool[i].transform.parent = _parentObject.transform;
+            ObjectPool[i].transform.position = _parentObject.transform.position;
+            ObjectPool[i].SetActive(false);
         }
     }
 
