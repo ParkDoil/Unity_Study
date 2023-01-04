0e433fa9bbd5be6f29d6564e0f6eb2fbd9216727
Park Doil <poker5323@naver.com>
2023-01-04 12:22:55 +0900

    [Feat] 레이캐스트 마우스 클릭 좌표 계산

---
+++ 23.01.04_MapTool/Assets/Scripts/CubeSpawnClick.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawnClick : MonoBehaviour
+{
+    private GameObject _selectObject;
+
+    private Vector3 _spawnPosition;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))  // 마우스가 클릭 되면
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("Map"))
+                {
+                    Debug.Log(hit.point);
+                    CheckPosition(hit.point);
+                }
+            }
+        }
+    }
+
+    public void SelectObject(GameObject _object)
+    {
+        _selectObject = _object;
+    }
+
+    private void CheckPosition(Vector3 _position)
+    {
+        #region X좌표 계산 구역
+        if (_position.x < -9)
+        {
+            _spawnPosition.x = -9;
+        }
+
+        else if (_position.x > 10)
+        {
+            _spawnPosition.x = 10;
+        }
+
+        else
+        {
+            _spawnPosition.x = Mathf.Round(_position.x);
+        }
+        #endregion
+        #region Z좌표 계산 구역
+        if (_position.z < -9)
+        {
+            _spawnPosition.z = -9;
+        }
+
+        else if (_position.z > 10)
+        {
+            _spawnPosition.z = 10;
+        }
+
+        else
+        {
+            _spawnPosition.z = Mathf.Round(_position.z);
+        }
+        #endregion
+    }
+}
